c54425bb83cf8ab1dd07094c004a32ee7a318c82
Rajin Hasnaine Khan <matiar@Rajins-MacBook-Pro.local>
2023-06-04 16:50:07 +0600

    Added graphtype headers and implementations

---
+++ Lab15/graphtype.h
@@ -0,0 +1,50 @@
+#ifndef GRAPHTYPE_H_INCLUDED
+#define GRAPHTYPE_H_INCLUDED
+
+#include "stacktype.h"
+#include "quetype.h"
+
+template<class VertexType>
+class GraphType {
+
+    public:
+
+        GraphType();
+        GraphType(int maxV);
+        ~GraphType();
+
+        void MakeEmpty();
+
+        bool IsEmpty();
+        bool IsFull();
+
+        void AddVertex(VertexType);
+        void AddEdge(VertexType, VertexType, int);
+
+        int WeightIs(VertexType, VertexType);
+
+        void GetToVertices(VertexType, QueType<VertexType>&);
+
+        void ClearMarks();
+        void MarkVertex(VertexType);
+
+        bool IsMarked(VertexType);
+
+        void DepthFirstSearch(VertexType, VertexType);
+        void BreadthFirstSearch(VertexType, VertexType);
+
+        int OutDegree(VertexType V);
+
+        bool FoundEdge(VertexType u, VertexType v);  // New member function
+
+
+    private:
+
+        int numVertices;
+        int maxVertices;
+        VertexType* vertices;
+        int **edges;
+        bool* marks;
+};
+
+#endif
